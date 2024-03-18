881d5547c9df2e7343d24c5ebeb48c6a5813a5bd
damiancxliew <damiancxliew@gmail.com>
2024-03-18 20:26:25 +0800

    Changes to be committed: 	modified:   client/src/api/config.ts

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,4 +1,5 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL ?? "https://5ea1-121-123-45-51.ngrok-free.app";
+  import.meta.env.VITE_API_URL ??
+  "https://e222-183-171-191-243.ngrok-free.app/";
 // "http://localhost:5000"
 // "https://rvscholar.netlify.app
