a1232dcf88adbdd5b06670e0d2e5eb747d761bec
damiancxliew <damiancxliew@gmail.com>
2024-03-18 10:15:55 +0800

    Changes to be committed: 	modified:   client/src/api/config.ts

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,3 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
+export const API_URL =
+  import.meta.env.VITE_API_URL ?? "https://rvscholar.netlify.app/";
 // "http://localhost:5000"
 // "https://rvscholar.netlify.app/"
